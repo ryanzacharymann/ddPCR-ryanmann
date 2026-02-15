f68af20db233cf3f8bcaabdac93ab250a69d62c1
Ryan Mann <ryan.zachary.mann@gmail.com>
2026-02-15 15:00:55 -0800

    allow -as any- in spec files to allow robust testing.

--- eslint.config.js
+++ eslint.config.js
@@ -5,40 +5,46 @@ const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
 module.exports = defineConfig([
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: ["app", "plate"],
-          style: "camelCase",
+    {
+        files: ["**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.recommended,
+            tseslint.configs.stylistic,
+            angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        rules: {
+            "@angular-eslint/directive-selector": [
+                "error",
+                {
+                    type: "attribute",
+                    prefix: ["app", "plate"],
+                    style: "camelCase",
+                },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                {
+                    type: "element",
+                    prefix: ["app", "plate"],
+                    style: "kebab-case",
+                },
+            ],
         },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: ["app", "plate"],
-          style: "kebab-case",
-        },
-      ],
     },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
+    {
+        files: ["**/*.html"],
+        extends: [
+            angular.configs.templateRecommended,
+            angular.configs.templateAccessibility,
+        ],
+        rules: {},
+    },
+    {
+        files: ["**/*.spec.ts", "**/*.test.ts"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }
 ]);
